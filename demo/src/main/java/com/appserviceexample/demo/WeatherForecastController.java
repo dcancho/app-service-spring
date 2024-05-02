5b051f77caf6a10f7b980c683ebc591c92c2a8c4
dcancho <102440160+dcancho@users.noreply.github.com>
2024-05-02 12:52:46 +0000

    Actualizar controlador de pronóstico del tiempo para devolver un objeto WeatherForecast en lugar de una cadena de texto

--- demo/src/main/java/com/appserviceexample/demo/WeatherForecastController.java
+++ demo/src/main/java/com/appserviceexample/demo/WeatherForecastController.java
@@ -5,18 +5,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherForecastController {
-	@RequestMapping
-	public String getWeatherForecast() {
-		String[] weatherConditions = {"sunny", "cloudy", "rainy", "snowy"};
-		int temperature = (int) (Math.random() * 50);
-		if (temperature > 30) {
-			return "The weather is " + weatherConditions[0] + " and the temperature is " + temperature + " degrees Celsius.";
-		} else if (temperature > 20) {
-			return "The weather is " + weatherConditions[1] + " and the temperature is " + temperature + " degrees Celsius.";
-		} else if (temperature > 10) {
-			return "The weather is " + weatherConditions[2] + " and the temperature is " + temperature + " degrees Celsius.";
-		} else {
-			return "The weather is " + weatherConditions[3] + " and the temperature is " + temperature + " degrees Celsius.";
-		}
-	}
+    @RequestMapping
+    public WeatherForecast getWeatherForecast() {
+        String[] weatherConditions = {"sunny", "cloudy", "rainy", "snowy"};
+        int temperature = (int) (Math.random() * 50);
+
+        WeatherForecast forecast = new WeatherForecast();
+        forecast.setTemperature(temperature);
+
+        if (temperature > 30) {
+            forecast.setCondition(weatherConditions[0]);
+        } else if (temperature > 20) {
+            forecast.setCondition(weatherConditions[1]);
+        } else if (temperature > 10) {
+            forecast.setCondition(weatherConditions[2]);
+        } else {
+            forecast.setCondition(weatherConditions[3]);
+        }
+
+        return forecast;
+    }
 }
